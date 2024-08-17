43516bccded96114e158255468a3f462b1cb6e59
saiteja-1304 <saitejapalle0506@gmail.com>
2024-08-17 10:02:12 +0530

    updated

--- src/main/java/com/ust/Surveys/service/SurveyService.java
+++ src/main/java/com/ust/Surveys/service/SurveyService.java
@@ -24,56 +24,14 @@ public class SurveyService {
     private AssessmentClient assessmentClient;
 
 
+    public SurveyFullResponse MapQuestions(Survey survey1) {
+        List<Integer> ques=survey1.getQuestionIds();
 
-//    public static class SurveyNotFoundException extends RuntimeException {
-//        public SurveyNotFoundException(String message) {
-//            super(message);
-//        }
-//    }
-
-//    public static class QuestionNotFoundException extends RuntimeException {
-//        public QuestionNotFoundException(String message) {
-//            super(message);
-//        }
-//    }
-
-//    public SurveyFullResponse addSurvey(Survey survey, List<Integer> ques) {
-//            //Survey survey1=survey.get();
-//        Survey survey1=surveyRepository.save(survey);
-//        //List<Integer> questionIds=ques;
-//            ResponseSetDto setInfos=assessmentClient.getAssessmentBySetName(survey1.getSetName());
-//            //setInfos.setQuestions(survey.getQuestions());
-//            SurveyFullResponse res= new SurveyFullResponse();
-//            res.setSetName(survey1.getSetName());
-//            res.setEmail(survey1.getEmail());
-//            res.setDomain(survey1.getDomain());
-//            res.setStatus(survey1.getStatus());
-//            res.setCompanyName(survey1.getCompanyName());
-//            //res.setQuestionIds(ques);
-//            List<SurveyQuestion> questions= new ArrayList<>();
-//            for (Integer questionId : ques) {
-//                List<SurveyQuestion> questionlist=setInfos.getQuestions();
-//                for(SurveyQuestion question:questionlist){
-//                    if(question.getQuestionId()==questionId){
-//                        questions.add(question);
-//                    }
-//                }
-//            }
-//            setInfos.setQuestions(questions);
-//            res.setSetinfos(setInfos);
-//            return res;
-//    }
-
-
-    public SurveyFullResponse MapQuestions(Survey survey) {
-        List<Integer> ques=survey.getQuestionIds();
-        // Check if survey is null
-        if (survey == null) {
+        if (survey1 == null) {
             throw new SurveyNotFoundException("Survey cannot be null");
         }
-        //survey.setQuestionIds(ques);
         // Save the survey to the repository
-        Survey survey1 = surveyRepository.save(survey);
+        //Survey survey1 = surveyRepository.save(survey);
 
         // Fetch assessment set info from the client
         ResponseSetDto setInfos = assessmentClient.getAssessmentBySetName(survey1.getSetName());
@@ -117,79 +75,10 @@ public class SurveyService {
     }
 
     public SurveyFullResponse addSurvey(Survey survey) {
-        SurveyFullResponse res = MapQuestions(survey);
-
-//        List<Integer> ques=survey.getQuestionIds();
-//        // Check if survey is null
-//        if (survey == null) {
-//            throw new SurveyNotFoundException("Survey cannot be null");
-//        }
-//        //survey.setQuestionIds(ques);
-//        // Save the survey to the repository
-//        Survey survey1 = surveyRepository.save(survey);
-//
-//        // Fetch assessment set info from the client
-//        ResponseSetDto setInfos = assessmentClient.getAssessmentBySetName(survey1.getSetName());
-//        if (setInfos == null) {
-//            throw new SurveyNotFoundException("Assessment set not found for setName: " + survey1.getSetName());
-//        }
-//
-//        // Initialize response object
-//        SurveyFullResponse res = new SurveyFullResponse();
-//        res.setSetName(survey1.getSetName());
-//        res.setEmail(survey1.getEmail());
-//        res.setDomain(survey1.getDomain());
-//        res.setStatus(survey1.getStatus());
-//        res.setCompanyName(survey1.getCompanyName());
-//
-//        // Process the questions
-//        List<SurveyQuestion> questions = new ArrayList<>();
-//        List<SurveyQuestion> questionlist = setInfos.getQuestions();
-//
-//        if (questionlist == null) {
-//            throw new SurveyNotFoundException("No questions found for the assessment set");
-//        }
-//
-//        for (Integer questionId : ques) {
-//            Optional<SurveyQuestion> questionOpt = questionlist.stream()
-//                    .filter(question -> question.getQuestionId().equals(questionId))
-//                    .findFirst();
-//
-//            if (!questionOpt.isPresent()) {
-//                throw new QuestionNotFoundException(questionId);
-//            }
-//
-//            questions.add(questionOpt.get());
-//        }
-//
-//        // Set the questions and return the response
-//        setInfos.setQuestions(questions);
-//        res.setSetinfos(setInfos);
-
+        Survey survey1 = surveyRepository.save(survey);
+        SurveyFullResponse res = MapQuestions(survey1);
         return res;
     }
-
-
-
-
-
-//    public SurveyFullResponse getSurvey(String surveyId) {
-//        Optional<Survey> survey = surveyRepository.findById(surveyId);
-//        if(survey.isPresent()){
-//            Survey survey1=survey.get();
-//            ResponseSetDto setInfos=assessmentClient.getAssessmentBySetName(survey1.getSetName());
-//            SurveyFullResponse res= new SurveyFullResponse();
-//            res.setSetName(survey1.getSetName());
-//            res.setEmail(survey1.getEmail());
-//            res.setDomain(survey1.getDomain());
-//            res.setStatus(survey1.getStatus());
-//            res.setCompanyName(survey1.getCompanyName());
-//            res.setSetinfos(setInfos);
-//            return res;
-//        }
-//        return null;
-//    }
-
 
     public SurveyFullResponse getSurvey(Long surveyId) {
         Optional<Survey> survey = surveyRepository.findById(String.valueOf(surveyId));
@@ -197,27 +86,12 @@ public class SurveyService {
             throw new SurveyNotFoundException("survey with " +surveyId+ " not found"); // Ensure this exception is defined
         }
         Survey survey1 = survey.get();
-        //if (survey.isPresent()) {
-//            Survey survey1 = survey.get();
-//            ResponseSetDto setInfos = assessmentClient.getAssessmentBySetName(survey1.getSetName());
-//            SurveyFullResponse res = new SurveyFullResponse();
-//            res.setSetName(survey1.getSetName());
-//            res.setEmail(survey1.getEmail());
-//            res.setDomain(survey1.getDomain());
-//            res.setStatus(survey1.getStatus());
-//            res.setCompanyName(survey1.getCompanyName());
-//            res.setSetinfos(setInfos);
-//            return res;
-
 
             List<Integer> ques=survey.get().getQuestionIds();
             // Check if survey is null
             if (survey == null) {
                 throw new SurveyNotFoundException("Survey cannot be null");
             }
-            //survey.setQuestionIds(ques);
-            // Save the survey to the repository
-            //Survey survey1 = surveyRepository.save(survey);
 
             // Fetch assessment set info from the client
             ResponseSetDto setInfos = assessmentClient.getAssessmentBySetName(survey1.getSetName());
@@ -252,7 +126,6 @@ public class SurveyService {
 
                 questions.add(questionOpt.get());
             }
-
             // Set the questions and return the response
             setInfos.setQuestions(questions);
             res.setSetinfos(setInfos);
@@ -260,24 +133,12 @@ public class SurveyService {
             return res;
 
         }
-        //throw new SurveyNotFoundException("survey with " +surveyId+ " not found"); // Throw custom exception if survey is not found
-
-
 
 
     public List<SurveyFullResponse> getAllSurveys() {
         List<SurveyFullResponse> fullResponses=new ArrayList<>();
         List<Survey> surveys = surveyRepository.findAll();
         for (Survey survey : surveys) {
-//            ResponseSetDto setInfos = assessmentClient.getAssessmentBySetName(survey.getSetName());
-//            SurveyFullResponse res = new SurveyFullResponse();
-//            res.setSetName(survey.getSetName());
-//            res.setEmail(survey.getEmail());
-//            res.setDomain(survey.getDomain());
-//            res.setStatus(survey.getStatus());
-//            res.setCompanyName(survey.getCompanyName());
-//            res.setSetinfos(setInfos);
-//            fullResponses.add(res);
             SurveyFullResponse res=MapQuestions(survey);
             fullResponses.add(res);
         }
